9d51a74d6b13b2adb1d935d779dd733efe1e83ec
Russo69max <juanpedrofreitas69@gmail.com>
2025-06-30 09:10:49 -0300

    Update calculo_de_imc.py

--- calculo_de_imc.py
+++ calculo_de_imc.py
@@ -1,6 +1,7 @@
 nome =  input("Qual o seu nome? ")
 peso = float(input("Qual é seu peso em quilos?"))
 altura = float(input("Qual a sua altura em metros?"))
+print("-------------------------------------------------")
 Faixa = print("Menor que 18.5  Abaixo do peso \n 18.5 a 24.9   Peso normal \n 25.0 a 29.9  Sobrepeso \n 30.0 a 34.9  Obesidade Grau I \n 35.0 a 39.9  Obesidade Grau II \n 40.0 ou mais   Obesidade Grau III (mórbida)")
 print("-------------------------------------------------")
 imc= (peso / (altura * altura))
